15453402476f1457d2d3b7cbdf5ed080d984d082
Petr <petr@kle.cz>
2023-06-12 21:10:07 +0200

    Formátování

--- generate.pl
+++ generate.pl
@@ -18,7 +18,6 @@ sub TeX::Hyphen::visualize {
         my ($self, $word, $separator) = (shift, shift, shift);
         my $number = 0;
         my $pos;
-
         for $pos ($self->hyphenate($word)) {
                 substr($word, $pos + $number, 0) = $separator;
                 $number = $number + length($separator);;
@@ -53,78 +52,78 @@ my %slovicka;
 my $wbr = '&shy;';
 
 foreach my $line (@content){
-	my ($en, $cz) = split /:/, $line;
-	my $fl = lc($en);
-	$fl =~ s/^(.).*/$1/;
-	if($abeceda{$fl}){
-		$abeceda{$fl}++;
-	}else{
-		$abeceda{$fl} = 1;
-	}
-	$slovicka{$fl}{$hypen->visualize($en, $wbr)} = $hypcz->visualize($cz, $wbr);
-	my $zbytek = $abeceda{$fl} % 6;
+  my ($en, $cz) = split /:/, $line;
+  my $fl = lc($en);
+  $fl =~ s/^(.).*/$1/;
+  if($abeceda{$fl}){
+    $abeceda{$fl}++;
+  }else{
+    $abeceda{$fl} = 1;
+  }
+  $slovicka{$fl}{$hypen->visualize($en, $wbr)} = $hypcz->visualize($cz, $wbr);
+  my $zbytek = $abeceda{$fl} % 6;
 }
 
 for my $pismeno (sort keys %abeceda) {
-	my $zbytek = $abeceda{$pismeno} % 6;
-	$pridat{$pismeno} = 6 - $zbytek;
+  my $zbytek = $abeceda{$pismeno} % 6;
+  $pridat{$pismeno} = 6 - $zbytek;
 }
 
 my $t = Template->new({
-		INCLUDE_PATH => $IN,
-		ENCODING => 'utf8',
-		VARIABLES => {
-		 version => $VERSION,
-		 appname => $appname,
-		 appshortname => $appshortname,
-		 appdesc => $appdesc,
+    INCLUDE_PATH => $IN,
+    ENCODING => 'utf8',
+    VARIABLES => {
+     version => $VERSION,
+     appname => $appname,
+     appshortname => $appshortname,
+     appdesc => $appdesc,
    },
 });
 
 for my $pismeno (sort keys %slovicka) {
-	$t->process('pismeno.html',
-		{ 
-			'title' => $appname . ' - '. uc $pismeno,
-			'slovicka' => $slovicka{$pismeno},
-			'pismeno' => $pismeno,
-			'pridat' => \%pridat,
-		},
-		"$OUT/$pismeno.html",
-		{ binmode => ':utf8' }) or die $t->error;
+  $t->process('pismeno.html',
+    {
+      'title' => $appname . ' - '. uc $pismeno,
+      'slovicka' => $slovicka{$pismeno},
+      'pismeno' => $pismeno,
+      'pridat' => \%pridat,
+    },
+    "$OUT/$pismeno.html",
+    { binmode => ':utf8' }) or die $t->error;
 }
 
 $t->process('index.html',
-	{ 
-		'title' => $appname,
-		'abeceda' => \%abeceda,
-		'aboutlink' => 1,
-	},
-	"$OUT/index.html",
-	{ binmode => ':utf8' }) or die $t->error;
+  {
+    'title' => $appname,
+    'abeceda' => \%abeceda,
+    'aboutlink' => 1,
+  },
+  "$OUT/index.html",
+  { binmode => ':utf8' }) or die $t->error;
 
 $t->process('about.html',
-	{ 
-		'title' => $appname,
-	},
-	"$OUT/about.html",
-	{ binmode => ':utf8' }) or die $t->error;
+  {
+    'title' => $appname,
+  },
+  "$OUT/about.html",
+  { binmode => ':utf8' }) or die $t->error;
 
 my @files = (
-	'slovicka.css',
-	'slovickadark.css',
-	'theme.js',
-	'slovicka.js',
+  'slovicka.css',
+  'slovickadark.css',
+  'theme.js',
+  'slovicka.js',
 );
 
 for my $file (@files){
-	$t->process($file ,{
-		'abeceda' => \%abeceda,
-	},
-	"$OUT/$file",
-		{ binmode => ':utf8' }) or die $t->error;
+  $t->process($file ,{
+    'abeceda' => \%abeceda,
+  },
+  "$OUT/$file",
+    { binmode => ':utf8' }) or die $t->error;
 }
 
 foreach my $file (glob("$IN/img/*")){
-	my ($name,$path) = fileparse($file);
-	copy("$path$name", "$OUT/$name");
+  my ($name,$path) = fileparse($file);
+  copy("$path$name", "$OUT/$name");
 }
