5303fe49ccb22c312452e180a7c2b9299aa6f5f7
Petr <petr@kle.cz>
2023-09-02 22:15:49 +0200

    Formátování

--- src/slovicka.js
+++ src/slovicka.js
@@ -2,14 +2,14 @@ var classname = document.getElementsByClassName("fis");
 
 var preklad = function() {
     var translation = this.getAttribute("data-translation");
-		var text = this.textContent;
-		this.textContent = translation;
-		this.setAttribute("data-translation", text);
-		if (this.className === "fis") {
-			this.className = "fis-vybrane";
-		}else{
-			this.className = "fis";
-		}
+    var text = this.textContent;
+    this.textContent = translation;
+    this.setAttribute("data-translation", text);
+    if (this.className === "fis") {
+      this.className = "fis-vybrane";
+    }else{
+      this.className = "fis";
+    }
 };
 
 for (var i = 0; i < classname.length; i++) {
